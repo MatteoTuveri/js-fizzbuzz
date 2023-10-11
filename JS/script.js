bdb76e3a369818986472f9608115b3c1897568c8
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-11 17:07:35 +0200

    Define console classes and add conditional for console generator in JS

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,25 @@
+//Console Generator
 
-for(let i = 1; i<=100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-document.getElementById('console').innerHTML += `<div class="debug box"> ${i} </div>`;
+    const box = document.getElementById('console');
+    box.innerHTML += `<div id="box-${i}" class="debug box"> ${i} </div>`;
+    const boxOption = document.getElementById(`box-${i}`);
+
+    if(i % 3 === 0 && i % 5 ===0){
+        boxOption.classList.add("fizz-buzz");
+        boxOption.innerHTML =`FizzBuzz`;
+    }
+    else if(i % 3 === 0 && i % 5 !==0){
+        boxOption.classList.add("fizz");
+        boxOption.innerHTML =`Fizz`;
+    }
+    else if(i % 3 !== 0 && i % 5 ===0){
+        boxOption.classList.add("buzz");
+        boxOption.innerHTML =`Buzz`;
+    }
+    else{
+        boxOption.classList.add("normal");
+    }
 
 }
